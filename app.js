3065f14550f63cab34eedd515ebd0cef27d10ea0
Harsh Lodhi <harshlodhi82@gmail.com>
2020-09-16 11:46:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const path = require('path');
+const app = express()
+const PORT = 5500
+
+app.get('/', (req, res) => {
+    res.send("This is Test API on Firebase.")
+})
+
+app.get('/json', (req, res) => {
+    res.json({ msg: "This is Test API on Firebase." })
+})
+
+app.listen(PORT, () => {
+    console.log(`Listioning on ${PORT}`);
+})
